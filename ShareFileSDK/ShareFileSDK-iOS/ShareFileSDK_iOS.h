efe0c156d712e3c24b18e9a6d33c17e7f9cf77ab
Fabien Lydoire <fabien.lydoire@fabtastic.world>
2019-11-26 10:24:36 +0100

    Added iOS SDK Framework

---
+++ ShareFileSDK/ShareFileSDK-iOS/ShareFileSDK_iOS.h
@@ -0,0 +1,21 @@
+//
+//  ShareFileSDK_iOS.h
+//  ShareFileSDK-iOS
+//
+//  Created by Fabien Lydoire on 25/11/2019.
+//  Copyright © 2019 Fabien Lydoire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShareFileSDK_iOS.
+FOUNDATION_EXPORT double ShareFileSDK_iOSVersionNumber;
+
+//! Project version string for ShareFileSDK_iOS.
+FOUNDATION_EXPORT const unsigned char ShareFileSDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShareFileSDK_iOS/PublicHeader.h>
+
+#import "ShareFileSDK.h"
+#import "SFASDKAuthHandler.h"
+#import "SFASDKAuthHandlerProtected.h"
